8c21f54e84add80fef8f4ee43e4ce421939fc738
Eri-random <brenherrera@alumno.unlam.edu.ar>
2023-07-17 14:34:43 -0300

    encriptando password

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
+import { User } from './entities/user.entity';
+import * as bcryptjs from 'bcryptjs';
 
 @Injectable()
 export class AuthService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new auth';
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    try {
+      const { password, ...userData } = createUserDto;
+      //1- Encriptar la contrseña
+      const newUser = new this.userModel({
+        password: bcryptjs.hashSync(password, 10),
+        ...userData,
+      });
+
+      //2- Guardar el usuario
+      await newUser.save();
+      const { password:_, ...user } = newUser.toJSON();
+
+      return user;
+    } catch (error) {
+      console.log(error.code);
+      if (error.code === 11000) {
+        throw new BadRequestException(`${createUserDto.email} already exists!`);
+      }
+      throw new InternalServerErrorException(`Something terribe happen`);
+    }
   }
 
   findAll() {
